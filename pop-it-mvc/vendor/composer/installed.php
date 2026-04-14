631fbe38eb057d7f35998f636e2814fc3350942c
Ростислав Грачев <gracevrostislav5@gmail.com>
2026-04-14 13:44:15 +0700

    feat: install composer and create basic project

---
+++ pop-it-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'repard7/practica-server',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '68be18c6d7a57ab3d3a8cd38a0dbe8aad097fe91',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'repard7/practica-server' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '68be18c6d7a57ab3d3a8cd38a0dbe8aad097fe91',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
