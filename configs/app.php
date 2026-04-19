6ba45696bf46b12669a04c4e19d7307726d6bebd
Ростислав Грачев <gracevrostislav5@gmail.com>
2026-04-19 22:04:43 +0700

    feat: add data filter

--- configs/app.php
+++ configs/app.php
@@ -11,6 +11,9 @@ return [
     'validators' => [
        'required' => \Validators\RequireValidator::class,
        'unique' => \Validators\UniqueValidator::class
-   ]
+    ],
+    'routeAppMiddleware' => [
+        'trim' => \Middlewares\TrimMiddleware::class,
+    ],
 
 ];
